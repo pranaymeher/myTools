c1ee200924daea44e194643300a919f2603e9749
= <=>
2020-03-22 19:50:22 -0700

    Added very first function

--- pm_utils.py
+++ pm_utils.py
@@ -1,1 +1,13 @@
+def findCenter(selList):
+    ''' Returns the center position of currently selected object'''
+    center = [0,0,0]
+    length = len(selList)
+    for i in selList:
+        pos = MC.xform(i, q=True, ws=True, translation=True)
+        center[0] += pos[0]
+        center[1] += pos[1]
+        center[2] += pos[2]
+    center = [center[0]/length, center[1]/length, center[2]/length]
+    return center
+
 
