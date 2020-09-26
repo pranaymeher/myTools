5e0e2559054e8b7fb687ef23836bfbf8f8e0624b
= <=>
2020-09-26 15:53:46 -0700

    Added match_points_position function

--- pm_utils.py
+++ pm_utils.py
@@ -1,5 +1,17 @@
+import maya.cmds as MC
+import maya.api.OpenMaya as om
+import time
+
+
 def findCenter(selList):
-    ''' Returns the center position of currently selected object'''
+    ''' Returns the center position of currently selected object or sub-objects
+	Arguments:
+		selList -- list containing names of the objects or sub-objects
+
+	Returns:
+		center -- type(float) center position of the selection
+
+    '''
     center = [0,0,0]
     length = len(selList)
     for i in selList:
@@ -11,3 +23,35 @@ def findCenter(selList):
     return center
 
 
+
+def match_points_position(objList, worldSpace=False):
+    ''' This function takes two objects and moves the vertices of
+    the second object to match the shape of the first object
+    
+    Arguments:
+        objList -- list containing names of the object to perform
+                    the operation on. Order of selection matters.
+                    First object will be the source and second will
+                    be the target.
+        worldSpace -- If True then the vertices of the target object
+                    will overlap on the vertices of the first object in worldspace.
+                    If False then the vertices will move in local space to match 
+                    the shape of the source object.
+                    
+    Returns -- None
+    
+    '''
+    if len(objList) != 2 :
+        MC.error('Requires exactly two objects. Stopping execution')
+    selList = om.MSelectionList()
+    selList.add(objList[0])
+    selList.add(objList[1])
+    mDagPath_source = selList.getDagPath(0)
+    mDagPath_target = selList.getDagPath(1)
+    space = om.MSpace.kWorld if worldSpace else om.MSpace.kObject
+    
+    meshFn = om.MFnMesh( mDagPath_source )
+    ppos = om.MPointArray()
+    ppos = meshFn.getPoints( space=space)
+    meshFn2 = om.MFnMesh( mDagPath_target)
+    meshFn2.setPoints(ppos, space=space)
