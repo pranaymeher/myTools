25e0390c585d3b4d4be793cfe92c93e0e50b2dea
= <=>
2020-09-26 16:58:19 -0700

    Signed-off-by: = <pranay meher>

--- pm_utils.py
+++ pm_utils.py
@@ -5,11 +5,11 @@ import time
 
 def findCenter(selList):
     ''' Returns the center position of currently selected object or sub-objects
-	Arguments:
-		selList -- list containing names of the objects or sub-objects
+    Arguments:
+        selList -- list containing names of the objects or sub-objects
 
-	Returns:
-		center -- type(float) center position of the selection
+    Returns:
+        center -- type(float) center position of the selection
 
     '''
     center = [0,0,0]
@@ -26,7 +26,8 @@ def findCenter(selList):
 
 def match_points_position(objList, worldSpace=False):
     ''' This function takes two objects and moves the vertices of
-    the second object to match the shape of the first object
+    the second object to match the shape of the first object.
+    This methos id 320 times faster than using normal maya.cmds script
     
     Arguments:
         objList -- list containing names of the object to perform
@@ -55,3 +56,43 @@ def match_points_position(objList, worldSpace=False):
     ppos = meshFn.getPoints( space=space)
     meshFn2 = om.MFnMesh( mDagPath_target)
     meshFn2.setPoints(ppos, space=space)
+
+
+
+def project_mesh(objList):
+    '''
+    This function projects the target mesh onto the source mesh
+    objList[0] = sourcemesh
+    objList[1] = targetmesh
+
+    Arguments:
+        objList -- [string] list containing exactly two names of meshes
+
+    Returns:
+        None
+    '''
+    
+    if len(objList) != 2:
+        MC.error('The objList needs to have exactly two objects')
+    else:
+        selList = om.MSelectionList()
+        selList.add(objList[0])
+        selList.add(objList[1])
+
+        target_dpath = selList.getDagPath(1)
+        geoIter = om.MItGeometry(target_dpath)
+        
+        source_dpath = selList.getDagPath(0)
+        mfnMesh_source = om.MFnMesh(source_dpath)
+        
+        pointArray = om.MPointArray()
+
+        while not geoIter.isDone():
+            pos = (geoIter.position())
+            c_pos,_ = mfnMesh_source.getClosestPoint(pos, om.MSpace.kWorld)
+            pointArray.append( [ c_pos[0], c_pos[1], c_pos[2] ])
+
+            geoIter.next()
+        
+        mfnMesh_target = om.MFnMesh(target_dpath)
+        mfnMesh_target.setPoints( pointArray, om.MSpace.kWorld )
